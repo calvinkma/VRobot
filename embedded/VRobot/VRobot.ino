d37b6a70592de61c1a7649a77ba5b2563fd8580c
Calvin Ma <43122403+calvinkma@users.noreply.github.com>
2019-02-17 13:51:32 -0500

    Update VRobot.ino

--- embedded/VRobot/VRobot.ino
+++ embedded/VRobot/VRobot.ino
@@ -6,26 +6,34 @@
 Servo servo;  // create servo object to control a servo
 RF24 radio(7, 2); // CE, CSN
 const byte address[6] = "00001";
-char text[1] = {0xFF};
+int ackData[2] = {12,23};
+int text = 0xFF;
 int Left_motor_go=0;
 int Left_motor_back=1; 
 int Right_motor_go=2;
 int Right_motor_back=3;
-
-int servo_pos = 90;    // variable to store the servo position
+bool aval=true;
+const uint64_t deviceID = 0xE8E8F0F0E1LL;
+int servo_pos = 180;    // variable to store the servo position
 
 void setup() {
   // RF Init
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1,deviceID);
+  //radio.startListening();  
+  radio.enableAckPayload();
+  radio.writeAckPayload(1, ackData, sizeof(ackData));
+  //radio.openReadingPipe(0, address);
+  //radio.setPALevel(RF24_PA_MIN);
+  //radio.startListening();
   
   // Servo Init
   servo.attach(6);  // attaches the servo on pin 6
   servo.write(servo_pos);
-
+  //Serial.println("sadf");
+  radio.startListening();
 }
 
 
@@ -58,7 +66,7 @@ void brake(int time)
   delay(time * 100);
 }
 
-void run(int time)
+void go(int time)
 {
   //digitalWrite(Right_motor_go,HIGH); 
   //digitalWrite(Right_motor_back,LOW);     
@@ -71,31 +79,50 @@ void run(int time)
   delay(time * 100);
 }
 //---------------Main---------------
-void loop() {
-   while(1){
-      if ( radio.available() ) {
-        radio.read(&text, sizeof(text));
-        Serial.println(text);
-        if (((int)text)!=0xFF){
-          break;}
-        }
-  }
-  if ( radio.available() ) {
-        radio.read(&text, sizeof(text));
-        Serial.println(text);
-}
-  servo.write((int)text);
-   if ( radio.available() ) {
-        radio.read(&text, sizeof(text));
-        Serial.println(text);
-}
-   if (((int)text)>0) {
-    run(10);
+void loop()
+{
+  while(1)
+  {
+    //Serial.println("1");
+    if (radio.available())
+    {
+      //Serial.println("2");
+      radio.read(&text, sizeof(text));
+      Serial.println(text);
+      if (text==0xFF)
+      {
+        delayMicroseconds(250);
+        break;
+      }
     }
-   if (((int)text)<0) {
+  }
+
+  while (!radio.available());
+  
+  radio.read(&text, sizeof(text));
+  //Serial.println(text);
+  delayMicroseconds(250);
+  
+  servo.write((int)text);
+  if (radio.available())
+  {
+    radio.read(&text, sizeof(text));
+    Serial.println("3");
+    //Serial.println(text);
+  }
+  
+  if (((int)text)>0)
+  {
+    go(10);
+  }
+  
+  if (((int)text)<0)
+  {
     back(10);
-   }
-   if (((int)text)==0) {
+  }
+  
+  if (((int)text)==0)
+  {
     brake(10);
-   }
+  }
 }
